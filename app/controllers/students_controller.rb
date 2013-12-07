7a21ede2e2dde1221effaa10d4de3a51055d84bc
Jason Swett <jason@electricsasquatch.com>
2013-12-06 20:13:37 -0500

    Fix student show page.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = params[:student]
+    @student = Student.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Hogwarts::Application.routes.draw do
   root to: 'welcome#index'
 
   get '/houses' => 'houses#List'
-  get '/students' => 'students#index'
-  get '/students' => 'students#creat'
+  resources :students
 end
